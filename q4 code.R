6df91fc6416097438c36d9c99b7cba94b878cbf5
penguins <119046507+1050960penguins@users.noreply.github.com>
2022-12-05 16:54:46 +0000

    Add files via upload

---
+++ q4 code.R
@@ -0,0 +1,30 @@
+#q 4 code
+
+
+setwd("~/Penguin_projects")
+penguins_raw <- read.csv("/Users/rozabailey/Penguin_projects/data_raw/penguins_raw.csv")
+#change to your working directory etc
+
+penguins_clean1 <- penguins_raw %>%
+  select(-starts_with("Delta")) %>%
+  select(-Comments)%>%
+  clean_names()
+#cleaning the data with a pipe to a new variable called penguins_clean1
+
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+#creating a colour blind friendly palette i can use for my figure
+
+penguins_clean1
+#linear model 
+penguin_mod1 <- lm(body_mass_g ~ flipper_length_mm, penguins_clean1)
+
+
+bodymass_flipper_plot<- ggplot(penguins_clean1, aes(x= body_mass_g, y=flipper_length_mm, colour = species))+
+  geom_point()+
+  scale_colour_manual(values=cbPalette)+
+  theme_bw()+
+  geom_smooth(method= "lm", col = "#009E73")+
+  labs(x= "Body Mass (g)", y = "Flipper Length (mm)")
+
+bodymass_flipper_plot
+#plotting a scattergraph of body mass vs flipper length with a linear model overlayed
